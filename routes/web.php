28dca6eba74b996a3070b6c498ce2465af0f28d3
Aliyya Putri <95596815+aliyyaps@users.noreply.github.com>
2022-06-08 08:28:48 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,8 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\PenyewaController;
 use App\Http\Controllers\MerkController;
-
+use App\Http\Controllers\MobilController;
+use App\Http\Controllers\PegawaiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,6 +40,8 @@ Route::middleware(['auth'])->group(function () {
     Route::middleware(['admin'])->group(function () {
         Route::resource('admin', AdminController::class);
         Route::resource('merk', MerkController::class);
+        Route::resource('merk', MobilController::class);
+        Route::resource('merk', PegawaiController::class);
     });
 
     Route::middleware(['penyewa'])->group(function () {
